22732419c372df352ce08947759e166d544550fe
Luka <87535465+LuxanL@users.noreply.github.com>
2024-07-03 17:25:11 +0200

    fixed spelling and corrected package name requirements.txt

--- whois-list.py
+++ whois-list.py
@@ -9,32 +9,34 @@ def checkDomain(domain, retries=3, delay=60):
             if domainInfo.creation_date == None and domainInfo.domain_name == None:
                 return True, None, None, None, None
             else:
-                return False, domainInfo.registrant_name, domainInfo.creation_date, domainInfo.updated_date, domainInfo.expiration_date
+                return (False, domainInfo.registrant_name, domainInfo.creation_date,
+                        domainInfo.updated_date, domainInfo.expiration_date)
         except Exception as e:
             logging.warning(f"Attempt {attempt + 1} failed for {domain}: {e}")
             time.sleep(delay)
+
 
 def main():
     txtInput = input("Input the domains txt filename path: ")
     with open(txtInput, "r") as file:
         domains = file.readlines()
 
-    txtOutput = input ("Enter output txt filename path: ")
+    txtOutput = input("Enter output txt filename path: ")
     if not txtInput:
         txtInput = "output"
 
     date = datetime.date.today().strftime('%Y-%m-%d')
 
-    with open(date + "-" + txtOutput + ".txt", "w") as outputFile:
+    with open(txtOutput + "-" + date + ".txt", "w") as outputFile:
         for domain in domains:
             domain = domain.strip()
-            avalable, registrant_name, creation_date, updated_date, expiration_date = checkDomain(domain)
-            if avalable is None:
+            available, registrant_name, creation_date, updated_date, expiration_date = checkDomain(domain)
+            if available is None:
                 output = f"Error checking {domain}: Max retries exceeded\n"
             else:
                 output = (
                     f"Domain: {domain}\n"
-                    f"Avalable: {avalable}\n"
+                    f"Available: {available}\n"
                     f"Registrant: {registrant_name}\n"
                     f"Creation Date: {creation_date}\n"
                     f"Updated Date: {updated_date}\n"
@@ -43,6 +45,7 @@ def main():
             logging.info(output.strip())
             outputFile.write(output + "\n")
             time.sleep(60)
+
 
 if __name__ == __name__:
     main()
